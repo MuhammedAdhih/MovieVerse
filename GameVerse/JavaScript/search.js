8e466d8c5d720aa18bb06c770f81afb2b3c1cd75
Adhih976305@gmail.com <adhih976305@gmail.com>
2026-08-05 20:43:08 +0530

    Move index.html to root

--- GameVerse/JavaScript/search.js
+++
@@ -1,9 +0,0 @@
-export function searchGames(query) {
-  const cards = document.querySelectorAll(".game-card");
-
-  cards.forEach((card) => {
-    const title = card.dataset.title.toLowerCase();
-
-    card.style.display = title.includes(query.toLowerCase()) ? "block" : "none";
-  });
-}
